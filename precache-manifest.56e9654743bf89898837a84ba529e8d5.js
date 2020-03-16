c330abad2034b395803467d736e7a6510472a903
Dave Nolan <dpn.nolan@gmail.com>
2020-03-16 13:00:33 -0400

    Updates

---
+++ precache-manifest.56e9654743bf89898837a84ba529e8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea65db5553deb49116c58e2c11f0904a",
+    "url": "/color-boxes-activity/index.html"
+  },
+  {
+    "revision": "aa8a0ffe943f46a49a38",
+    "url": "/color-boxes-activity/static/css/main.0a3793e8.chunk.css"
+  },
+  {
+    "revision": "00343cab4bba16868bcc",
+    "url": "/color-boxes-activity/static/js/2.e47fef4a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/color-boxes-activity/static/js/2.e47fef4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8a0ffe943f46a49a38",
+    "url": "/color-boxes-activity/static/js/main.13799aee.chunk.js"
+  },
+  {
+    "revision": "3f9fbaf118ca7e04edaf",
+    "url": "/color-boxes-activity/static/js/runtime-main.d46f96eb.js"
+  }
+]);
